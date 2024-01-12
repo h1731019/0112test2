24f6a2e1e4920d362566c13c039b51ccbe73fe20
h1731019 <h1731019@gmail.com>
2024-01-12 16:11:37 +0900

    tets2

---
+++ Main.java
@@ -0,0 +1,14 @@
+package rensyuu_9;
+
+import java.util.ArrayList;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("1円以上のアイテム一覧を表示します");
+		ArrayList<Item> items = ItemsDAO.findByMinimumPrice(1);
+		for(Item item : items) {
+			System.out.println(item.getName()+item.getPrice()+item.getWeight());
+			System.out.println("");
+		}
+	}
+}
